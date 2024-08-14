d9b6184d8dee3dc1b4e85ad920ee6b6cf6eecdd8
LiangWenshuo1118 <73432685+LiangWenshuo1118@users.noreply.github.com>
2024-08-14 14:40:38 +0800

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,30 @@
+salt_system = {
+  system1:{
+    system:['KNO3','NaNO3','NaCl']，
+    mol_ratio:[22, 50, 28]，
+    tempurature_density:[
+      [400, 2.20],
+      [420, 2.31],
+      [430, 2.40],
+      [440, 2.45]]，
+    tempurature_viscosity:[
+      [400, 0.30],
+      [420, 0.28],
+      [430, 0.26],
+      [440, 0.25]]，
+  }，
+  system2:{
+    system:['KNO3','NaNO3','NaCl']，
+    mol_ratio:[22, 55, 23]，
+    tempurature_density:[
+      [400, 2.22],
+      [420, 2.33],
+      [430, 2.42],
+      [440, 2.47]]，
+    tempurature_viscosity:[
+      [400, 0.31],
+      [420, 0.29],
+      [430, 0.27],
+      [440, 0.26]]，
+  }    
+}
